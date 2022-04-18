daf62b88e6ac9fd5077a3c78a107c2a9b124d5fa
Lais <laismassacesi@gmail.com>
2022-04-18 14:25:52 -0300

    Implementa função que verifica idade mínima dos animais

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return data.species.find((eachObject) => (eachObject.name === animal)).residents.every((resident) => resident.age > age);
+  return data.species.find((eachObject) => (eachObject.name === animal))
+    .residents.every((resident) => resident.age > age);
 }
 console.log(getAnimalsOlderThan('lions', 20));
 
