ef6f1cb28554131083b06ca5a92e16ee5c5c4100
Lais <laismassacesi@gmail.com>
2022-04-19 22:24:49 -0300

    Função countAnimals implementada

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const getResidents = (animal) => {
+  const arrayResidents = data.species.filter((eachObj) => animal.specie === eachObj.name);
+  return arrayResidents[0].residents;
+};
+
+const getResidentsCountByGender = (animal) => {
+  const arrayKeys = Object.keys(animal);
+  if (arrayKeys.includes('sex')) {
+    const allResidents = getResidents(animal);
+    if (animal.sex === 'female') {
+      console.log('é femea');
+      return allResidents.filter((eachOne) => eachOne.sex === 'female').length;
+    }
+    if (animal.sex === 'male') {
+      console.log('é macho');
+      return allResidents.filter((eachOne) => eachOne.sex === 'male').length;
+    }
+  }
+};
+function countAnimals(animal = {}) {
+  const arrayKeys = Object.keys(animal);
+  if (arrayKeys.includes('specie') && !arrayKeys.includes('sex')) {
+    return data.species.find((eachOne) => eachOne.name === animal.specie).residents.length;
+  }
+  if (arrayKeys.includes('sex')) return getResidentsCountByGender(animal);
+  const array = data.species.map((specie) => ({ [specie.name]: specie.residents.length }));
+  return Object.assign({}, ...array);
 }
+console.log(countAnimals({ specie: 'lions', sex: 'female' }));
 
 module.exports = countAnimals;
